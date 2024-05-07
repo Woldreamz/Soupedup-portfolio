1c56b048f1e5924720a483bbdb06cca5dc80ee0c
Rainer Nsa <woldreamz@gmail.com>
2024-05-07 21:48:14 +0100

    City of God

--- app/page.tsx
+++ app/page.tsx
@@ -12,15 +12,14 @@ export default function Home() {
       >
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">
-            Make anything possible with
+          First of His Name Master of Algorithms
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
               {" "}
-              Web Development
+              Guardian of Digital Realms
             </span>
           </h1>
           <p className="text-gray-200 hidden md:block">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Cross-platfom Developer, using state of the art tech stacks. 
           </p>
           <div className="flex-col md:flex-row hidden md:flex gap-5">
             <Link
